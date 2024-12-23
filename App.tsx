03607edb8690c0a764eb8660f7374e15546597cd
Dylan Feuerman <dfeuerm1@students.towson.edu>
2024-12-23 00:03:02 -0500

    modified:   App.tsx 	modified:   android/build.gradle 	modified:   android/gradle.properties 	modified:   package-lock.json 	modified:   package.json 	modified:   screens/Login.tsx 	modified:   src/amplifyconfiguration.json 	deleted:    yarn.lock

--- App.tsx
+++ App.tsx
@@ -1,37 +1,13 @@
-// App.js
-import { Amplify } from 'aws-amplify';
-import awsConfig from './src/aws-exports';
-
-Amplify.configure(awsConfig);
-
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import LoginScreen from './src/LoginScreen';
-
-export default function App() {
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <LoginScreen />
-    </SafeAreaView>
-  );
-}
-
-
-
-/*
 import React from "react";
 import { Button, View, StyleSheet, SafeAreaView } from "react-native";
 
-import { Amplify } from 'aws-amplify';
+import { Amplify } from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
 
-import awsconfig from './src/aws-exports.js';  
+import outputs from "./amplify_outputs.json";
 
-Amplify.configure({
-  ...awsconfig,
-  logging: { level: 'DEBUG' }
-});
-Amplify.configure(awsconfig);
+Amplify.configure(outputs);
+
 const SignOutButton = () => {
   const { signOut } = useAuthenticator();
 
@@ -61,47 +37,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-/*import { View, Text, Button, StyleSheet } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import React, { useEffect, useState } from 'react';
-// Import your screens
-import HomeScreen from './screens/HomeScreen';
-import Login from './screens/Login';
-import VisitPage from './screens/VisitScreen';
-import PostVisitPage from './screens/PostVisitScreen';
-import { UserProvider } from './data/loadData';
-import { PatchAppointmentRequest } from './src/hooks/useFetchAppointmentData';
-import { useForm, FormProvider } from 'react-hook-form';
-import { AuthProvider } from './src/contexts/AuthProvider';
-const Stack = createStackNavigator();
-import config from './amplifyconfiguration.json'
-import {withAuthenticator} from 'aws-amplify-react-native'
-
-import {Amplify} from 'aws-amplify';
-Amplify.configure(config)
-function App() {
-  return (
-    <AuthProvider>
-    <UserProvider>
-      <SafeAreaProvider>
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Login"
-          screenOptions={{
-            headerShown: false,
-            cardStyle: { backgroundColor: '#F5F5F7' }, // Set your desired background color here
-          }}>
-            <Stack.Screen name="Login" component={Login}/>
-            <Stack.Screen name="Home" component={HomeScreen}/>
-            <Stack.Screen name="VisitPage" component={VisitPage}/>
-            <Stack.Screen name="PostVisitPage" component={PostVisitPage}/>
-          </Stack.Navigator>
-        </NavigationContainer>
-      </SafeAreaProvider>
-    </UserProvider>
-    </AuthProvider>
-  );
-}
-
-export default App;*/
--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,28 +1,19 @@
 import React from 'react';
-import { View, Image, StyleSheet, Dimensions } from 'react-native';
+import { View, Image, StyleSheet, useWindowDimensions } from 'react-native';
 import LoginForm from '../components/forms/LoginForm';
 
-const { height, width } = Dimensions.get('window');
+const Login = () => {
+  const { width } = useWindowDimensions();
+  const isSmallScreen = width < 768; // Mimics the `base` and `md` breakpoints in Chakra UI
 
-export const Login = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <View style={[styles.flex, styles.imageContainer]}>
-        {/*
-        <Image
-          source={require('../../assets/images/login-background.png')}
-          style={styles.backgroundImage}
-        />
-        */}
-      </View>
-      <View style={[styles.flex, styles.loginContainer]}>
-        {/*
-        <Image
-          source={require('../../assets/images/logo_white.svg')}
-          style={styles.logo}
-        />
-        */}
-        <LoginForm navigation={navigation} />
+      {!isSmallScreen && (
+        <View style={styles.backgroundContainer}>
+        </View>
+      )}
+      <View style={styles.loginContainer}>
+        <LoginForm />
       </View>
     </View>
   );
@@ -31,27 +22,29 @@ export const Login = ({navigation}) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-  },
-  flex: {
-    flex: 1,
-  },
-  imageContainer: {
-    display: 'none',
-  },
-  backgroundImage: {
+    flexDirection: 'row',
     height: '100%',
     width: '100%',
-    resizeMode: 'cover',
+  },
+  backgroundContainer: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
   loginContainer: {
+    flex: 1,
     backgroundColor: '#0A1827',
-
+    justifyContent: 'center',
+    alignItems: 'center',
   },
   logo: {
-    height: 100,
-    resizeMode: 'contain',
+    height: 150,
+    marginBottom: 20,
   },
 });
 
 export default Login;
+
